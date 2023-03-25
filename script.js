0102d37805481a0936ad43340b0ec4238841dc15
Logan Pendergraft <ldpendergraft@gmail.com>
2023-03-25 12:04:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function hide(id){
+    document.getElementById(id).remove()
+}
+function hoverIn(img){
+    img.src = "images/succulents-2.jpg"
+}
+function hoverOut(img){
+    img.src = "images/succulents-1.jpg"
+}
